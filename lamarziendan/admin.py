ebb9dc9fd9914390bcb72c3e5410198369869066
Jaap Joris Vens <jj@rtts.eu>
2018-10-28 18:12:41 +0100

    admin live

--- lamarziendan/admin.py
+++ lamarziendan/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class PerformanceAdmin(admin.StackedInline):
+    model = Performance
+    extra = 0
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Edition)
+class EditionAdmin(admin.ModelAdmin):
+    inlines = [PerformanceAdmin]
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    inlines = [PerformanceAdmin]
--- lamarziendan/models.py
+++ lamarziendan/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from numberedmodel.models import NumberedModel
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField('naam', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+        ordering = ['name']
+
+class Artist(models.Model):
+    name = models.CharField('naam', max_length=255)
+    description = RichTextField('beschrijving', blank=True)
+    image = models.ImageField('afbeelding', blank=True)
+    links = models.TextField('social media links', help_text='Plak hier per regel één hyperlink', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Artiest'
+        verbose_name_plural = 'Artiesten'
+        ordering = ['name']
+
+class Edition(models.Model):
+    date = models.DateField()
+    title = models.CharField('titel', max_length=255)
+    description = RichTextField('beschrijving', blank=True)
+    image = models.ImageField('afbeelding', blank=True)
+    photolink = models.URLField('link naar het fotoalbum', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Editie'
+        verbose_name_plural = 'Edities'
+        ordering = ['date']
+
+class Performance(NumberedModel):
+    number = models.PositiveIntegerField('nummer', blank=True)
+    artist = models.ForeignKey(Artist, verbose_name='artiest', on_delete=models.CASCADE, related_name='performances')
+    edition = models.ForeignKey(Edition, verbose_name='editie', on_delete=models.CASCADE, related_name='performances')
+    genre = models.ForeignKey(Genre, verbose_name='genre', on_delete=models.CASCADE, related_name='+')
+
+    def __str__(self):
+        return '{} op {}'.format(self.artist, self.edition)
+
+    def number_with_respect_to(self):
+        return self.edition.performances.all()
+
+    class Meta:
+        verbose_name = 'Optreden'
+        verbose_name_plural = 'Optredens'
+        ordering = ['number']
